25014c8c7794b503b5bfb0b4eec7884b898d8c24
Paweł Łęczycki <pawel@msd.info.pl>
2020-11-16 11:49:26 +0000

    Interface Segregation Principle

---
+++ WPCSharp/SOLID/IV_ISP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WPCSharp.SOLID.I
+{
+    public interface IPdfFormatter
+    {
+        void ToPdf();
+    }
+    public interface IExcelFormatter
+    {
+        void ToExcel();
+    }
+
+    public class Report : IPdfFormatter, IExcelFormatter
+    {
+        public void ToExcel()
+        {
+            Console.WriteLine("Generating Excel...");
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Generating Pdf...");
+        }
+    }
+
+    public class Poem : IPdfFormatter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Generating Pdf...");
+        }
+    }
+}
