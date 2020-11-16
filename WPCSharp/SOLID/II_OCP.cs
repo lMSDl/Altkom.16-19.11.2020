2e786b623b8871f62b99ed4309cd271110819fae
Paweł Łęczycki <pawel@msd.info.pl>
2020-11-16 11:14:51 +0000

    Open-Closed Principle

---
+++ WPCSharp/SOLID/II_OCP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WPCSharp.SOLID.O
+{
+    internal abstract class Shape
+    {
+        public abstract double Area { get; }
+    }
+
+    internal class Square : Shape
+    {
+        public int A { get; set; }
+        public override double Area => A * A;
+    }
+
+    internal class Rectangle : Shape
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public override double Area => A * B;
+    }
+
+    internal class Circle : Shape
+    {
+        public int R { get; set; }
+        public override double Area => Math.PI * R * R;
+    }
+
+    internal class ShapeCalculator
+    {
+        private double Area(Shape shape)
+        {
+            return shape.Area;
+        }
+    }
+}
---
+++ WPCSharp/SOLID/II_OCP_Exercise.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPCSharp.SOLID.O
+{
+    public class TaxCalculator
+    {
+        public double CalculateTax(double Price) { return Price * 0.22; }
+    }
+    public class Item
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+    }
+    public class CashRegister
+    {
+        public TaxCalculator taxCalc = new TaxCalculator();
+        public double CalculatePrice(Item[] Items)
+        {
+            double _price = 0;
+            foreach (Item item in Items)
+            {
+            _price += item.Price + taxCalc.CalculateTax(item.Price);
+            }
+            return _price;
+        }
+        public void PrintBill(Item[] Items)
+        {
+            foreach (var item in Items)
+                Console.WriteLine("towar {0} : cena {1} + podatek {2}",
+                item.Name, item.Price, taxCalc.CalculateTax(item.Price));
+        }
+    }
+}
