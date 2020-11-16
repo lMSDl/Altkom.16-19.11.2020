9d43c4de2f197866c6635e5c2a29c8b7003346bf
Paweł Łęczycki <pawel@msd.info.pl>
2020-11-16 09:42:37 +0000

    TDA

--- WPCSharp/DesignPrinciples/PaymentService.cs
+++ WPCSharp/DesignPrinciples/PaymentService.cs
@@ -9,14 +9,8 @@ namespace WPCSharp.DesignPrinciples
 
         public bool Charge(int customerId, float amount)
         {
-            if (GetBalance(customerId) < amount)
-            {
-                return false;
-            }
             PaymentAccount customer = FindCustomerById(customerId);
-
-            customer.Outcome += amount;
-            return true;
+            return customer?.Charge(amount) ?? false;
         }
 
         private PaymentAccount FindCustomerById(int customerId)
@@ -27,18 +21,7 @@ namespace WPCSharp.DesignPrinciples
         public void Fund(int customerId, float amount)
         {
             PaymentAccount customer = FindCustomerById(customerId);
-            if (customer == null)
-            {
-                return;
-            }
-
-            customer.Income += amount;
-        }
-
-        public float? GetBalance(int customerId)
-        {
-            PaymentAccount customer = FindCustomerById(customerId);
-            return customer?.Income - customer?.Outcome;
+            customer?.Fund(amount);
         }
     }
 }
