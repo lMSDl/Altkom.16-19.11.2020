aa422858f5030d81be705bde2c889992fbd0caf6
Paweł Łęczycki <pawel@msd.info.pl>
2020-11-16 12:03:24 +0000

    Dependency Inversion Principle

---
+++ WPCSharp/SOLID/V_DIP.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPCSharp.SOLID.D
+{
+    public interface IMessage
+    {
+        void Send();
+    }
+
+    public class SMS : IMessage
+    {
+        public string Number { get; set; }
+        public string Content { get; set; }
+
+        public void Send()
+        {
+            Console.WriteLine("Sending SMS...");
+        }
+    }
+    public class MMS : IMessage
+    {
+        public string Number { get; set; }
+        public byte[] Content { get; set; }
+
+        public void Send()
+        {
+            Console.WriteLine("Sending MMS...");
+        }
+    }
+    public class Email : IMessage
+    {
+        public string Address { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public void Send()
+        {
+            Console.WriteLine("Sending Email...");
+        }
+    }
+
+    public class Messanger
+    {
+        public IEnumerable<IMessage> Messages { get; set; }
+
+        public void Send()
+        {
+            Messages.ToList().ForEach(x => x.Send());
+        }
+    }
+}
