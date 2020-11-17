809e3cf5829d2b4f1910663f1a28702698d246c4
Paweł Łęczycki <pawel@msd.info.pl>
2020-11-17 14:57:09 +0000

    private class data

---
+++ WPCSharp/DesignPatterns/Structural/PrivateClassData/DataClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPCSharp.DesignPatterns.Structural
+{
+    public class DataClass
+    {
+        private int attr1;
+        private int attr2;
+
+        public DataClass(int attr1, int attr2)
+        {
+            this.attr1 = attr1;
+            this.attr2 = attr2;
+        }
+
+        public int GetAttr1()
+        {
+            return attr1;
+        }
+
+        public int GetAttr2()
+        {
+            return attr2;
+        }
+    }
+}
---
+++ WPCSharp/DesignPatterns/Structural/PrivateClassData/SomeClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPCSharp.DesignPatterns.Structural
+{
+    class SomeClass
+    {
+        //private DataClass dataClass;
+
+        private int Attr1 { get; }
+        private int Attr2 { get; }
+        public SomeClass(int attr1, int attr2)
+        {
+            Attr1 = attr1;
+            Attr2 = attr2;
+            //dataClass = new DataClass(attr1, attr2);
+        }
+
+        public override string ToString()
+        {
+            //return $"{dataClass.GetAttr1()} {dataClass.GetAttr2()}";
+            return $"{Attr1} {Attr2}";
+        }
+    }
+}
